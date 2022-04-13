270be61923e070b9b69b586669d113f5dc47a915
kimmi1206 <kheinerimm1206@gmail.com>
2022-04-12 21:04:56 -0500

    error-handling

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof RequestValidationError) {
+    //console.log('Handling this error as a Request validation error!', err);
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    //console.log('Handling this error as a db connection error!', err);
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
+
+  res.status(400).send({
+    errors: [{ message: 'Something went wrong' }],
+  });
+};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,48 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  '/api/users/signup',
+  [
+    body('email') // .isEmail()
+      .isEmail()
+      .withMessage('Please enter a valid email address'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Please enter a password between 4 and 20 characters'),
+    // body('confirmPassword')
+    //   .trim()
+    //   .custom((value, { req }) => {
+    //     if (value !== req.body.password) {
+    //       throw new Error('Passwords do not match');
+    //     }
+    //     return true;
+    //   })
+    //   .withMessage('Passwords do not match'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
 
-  //  res.send('Hi there!');
-});
+      //  --other examples
+      // return res.status(400).json({ errors: errors.array() });
+      //return res.status(400).send(errors.array());
+      // const error = new Error('Invalid Email or Password');
+      // error.reasons = errors.array();
+      // throw error;
+    }
+    const { email, password } = req.body;
+    console.log('Creating user with email: ' + email);
+    throw new DatabaseConnectionError();
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
