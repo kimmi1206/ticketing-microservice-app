d80dc65e54f835c2a72d4070511f1e3f11bcc955
kimmi1206 <kheinerimm1206@gmail.com>
2022-04-16 17:20:30 -0500

    added nextjs client

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await axios.post('/api/users/signup', {
+      email,
+      password,
+    });
+
+    console.log(response.data);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+          type="email"
+          placeholder="Enter email"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-control"
+          type="password"
+          placeholder="Enter password"
+        />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
