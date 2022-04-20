4df85b985f6ae28249242a9dfe1c9e7fb8a4a93f
kimmi1206 <kheinerimm1206@gmail.com>
2022-04-20 13:44:22 -0500

    added tickets services and deployment files

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,15 +4,18 @@ import { app } from './app';
 const start = async () => {
   // only for local testing
   process.env.JWT_KEY = 'mysecretkey_for-testing_in_dev-env'; // only for local testing in dev environment
+  // process.env.MONGO_URI = 'mongodb://localhost:27017/auth' // for testing locally
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   try {
-    // for testing locally
-    // await mongoose.connect('mongodb://localhost:27017/auth', {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
